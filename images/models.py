e53422d4130a5d8c6152d655b643744739663dc8
Lukasz Burdzy <lukaszburdzy@gmail.com>
2016-06-23 00:41:07 +0200

    model change

--- images/models.py
+++ images/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django_extensions import TimeStampedModel
 
-# Create your models here.
+
+class Image(TimeStampedModel):
+    title = models.CharField(max_length=200, verbose_name='title')
+    file = models.ImageField(verbose_name='image file')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'image'
+        verbose_name_plural = 'images'
