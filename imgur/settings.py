6a3a1f8a20ec8a58a653c625c77b7fd37846342b
Lukasz Burdzy <lukaszburdzy@gmail.com>
2016-06-27 00:57:19 +0200

    Staticfiles time to switch to feature branching?

--- imgur/settings.py
+++ imgur/settings.py
@@ -63,7 +63,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'imgur.templates'
+            'imgur/templates/',
+            os.path.join(BASE_DIR, 'templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -128,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static_dir'),)
